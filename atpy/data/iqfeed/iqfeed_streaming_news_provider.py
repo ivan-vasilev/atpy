425e21f2fc731a8b30c19474809f871749b460a1
Ivan Vasilev <vasilev@exb.de>
2017-01-02 15:31:54 +0200

    Streaming news work-in-progress

--- atpy/data/iqfeed/iqfeed_streaming_news_provider.py
+++ atpy/data/iqfeed/iqfeed_streaming_news_provider.py
@@ -23,24 +23,26 @@ class IQFeedStreamingNewsProvider(IQFeedBaseProvider, SilentQuoteListener):
         return self
 
     def process_news(self, news_item: QuoteConn.NewsMsg) -> None:
-        self.queue.put(news_item.story_id, news_item.headline, news_item.story_time, news_item.symbol_list,
-                       news_item.distributor)
+        self.queue.put(news_item)
 
     def __del__(self):
         if self.quote_conn is not None and self.quote_conn.connected():
             self.quote_conn.disconnect()
 
     def __next__(self) -> map:
-        result = {'id' + self.key_suffix: list(), 'headline' + self.key_suffix: list(),
+        result = {'id' + self.key_suffix: list(),
+                  'headline' + self.key_suffix: list(),
+                  'date' + self.key_suffix: list(),
                   'time' + self.key_suffix: list(),
                   'symbols' + self.key_suffix: list(),
                   'distributor' + self.key_suffix: list()}
 
-        for i, (story_id, headline, date, time, symbols, distributor) in enumerate(iter(self.queue.get, None)):
+        for i, (story_id, distributor, symbol_list, story_date, story_time, headline) in enumerate(iter(self.queue.get, None)):
             result['id' + self.key_suffix].append(story_id)
             result['headline' + self.key_suffix].append(headline)
-            result['time' + self.key_suffix].append(time)
-            result['symbols' + self.key_suffix].append(symbols)
+            result['date' + self.key_suffix].append(story_date)
+            result['time' + self.key_suffix].append(story_time)
+            result['symbols' + self.key_suffix].append(symbol_list)
             result['distributor' + self.key_suffix].append(distributor)
 
             if (i + 1) % self.minibatch == 0:
