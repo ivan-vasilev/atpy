f45a75c0b5910d0ff96cb58826874fd115b5e7a4
Ivan Vasilev <vasilev@exb.de>
2017-08-20 19:12:24 +0300

    latest bars modification

--- tests/iqfeed/test_latest_bars.py
+++ tests/iqfeed/test_latest_bars.py
@@ -53,5 +53,52 @@ class TestIQFeedBarData(unittest.TestCase):
             e2.wait()
             e3.wait()
 
+    def test_2(self):
+        with IQFeedLatestBars(mkt_snapshot_depth=3, interval_len=300) as listener:
+            # test bars
+            ticker = ['MMM', 'AXP', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'KO', 'DO', 'XOM', 'GE', 'GS', 'HD', 'IBM', 'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT', 'NKE', 'PFE', 'PG', 'TRV', 'UNH', 'UTX', 'VZ', 'V', 'WMT', 'DIS']
+
+            e1 = {t: threading.Event() for t in ticker}
+            counters = {t: 0 for t in ticker}
+
+            def bar_listener(event):
+                self.assertTrue(event['data']['Symbol'] in ticker)
+                counters[event['data']['Symbol']] += 1
+                if counters[event['data']['Symbol']] >= listener.mkt_snapshot_depth:
+                    e1[event['data']['Symbol']].set()
+
+            listener.on_bar += bar_listener
+
+            # test market snapshot
+            e2 = threading.Event()
+            e3 = threading.Event()
+
+            def on_market_snapshot(event):
+                self.assertEqual(event['data'].shape[1], 9)
+                e2.set()
+                if len(event['data'].index.levels[0]) == 4:
+                    e3.set()
+
+            listener.on_market_snapshot += on_market_snapshot
+
+            mkt_snapshot = events.after(lambda: {'type': 'request_market_snapshot_bars'})
+            mkt_snapshot += listener.on_event
+
+            watch_bars = events.after(lambda: {'type': 'watch_bars', 'data': {'symbol': ticker, 'update': 1}})
+            watch_bars += listener.on_event
+            watch_bars()
+
+            watch_history_bars = events.after(lambda: {'type': 'watch_history_bars', 'data': ['AAPL', 'FB']})
+            watch_history_bars += listener.on_event
+            watch_history_bars()
+
+            for e in e1.values():
+                e.wait()
+
+            mkt_snapshot()
+
+            e2.wait()
+            e3.wait()
+
 if __name__ == '__main__':
     unittest.main()
