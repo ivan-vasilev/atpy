eee05576b359186587b3cce41dc9c6ad1757eb1d
Ivan Vasilev <ivanvasilev@gmail.com>
2018-03-01 03:52:33 +0200

    minor performance improvements

--- atpy/data/ts_util.py
+++ atpy/data/ts_util.py
@@ -26,6 +26,10 @@ def set_periods(df: pd.DataFrame):
     lc.apply(a, axis=1)
 
 
+__open_and_closes_series = pd.concat([tcal.open_and_closes['market_open'], tcal.open_and_closes['market_close']]).sort_values()
+__closes_series = tcal.open_and_closes['market_close']
+
+
 def current_period(df: pd.DataFrame):
     """
     Slice only the current period (e.g. trading hours or after hours)
@@ -33,20 +37,25 @@ def current_period(df: pd.DataFrame):
     :return sliced period
     """
 
-    lc = tcal.open_and_closes.loc[df.iloc[0].timestamp.date():df.iloc[-1].timestamp.date()]
-    lc = pd.concat([lc['market_open'], lc['market_close']]).sort_values()[::-1]
+    most_recent = df.iloc[-1].timestamp
 
     xs = pd.IndexSlice
 
-    for i in range(len(lc) + 1):
-        if i == 0:
-            result, period = df.loc[xs[lc.iloc[i]:, :] if isinstance(df.index, pd.MultiIndex) else xs[lc.iloc[i]:]], 'after-hours'
-        elif i == len(lc):
-            result, period = df.loc[xs[:lc.iloc[-1], :] if isinstance(df.index, pd.MultiIndex) else xs[:lc.iloc[-1]]], 'after-hours'
-        elif i % 2 == 0:
-            result, period = df.loc[xs[lc.iloc[i]:lc.iloc[i - 1], :] if isinstance(df.index, pd.MultiIndex) else xs[lc.iloc[i]:lc.iloc[i - 1]]], 'after-hours'
-        elif i % 2 == 1:
-            result, period = df.loc[xs[lc.iloc[i]:lc.iloc[i - 1], :] if isinstance(df.index, pd.MultiIndex) else xs[lc.iloc[i]:lc.iloc[i - 1]]], 'trading-hours'
+    if most_recent.date() in tcal.trading_days:
+        current_hours = __open_and_closes_series.loc[most_recent.date()]
+        if most_recent > current_hours[1]:
+            result, period = df.loc[xs[current_hours[1]:, :] if isinstance(df.index, pd.MultiIndex) else xs[current_hours[1]:]], 'after-hours'
+        else:
+            result, period = df.loc[xs[current_hours[0]:, :] if isinstance(df.index, pd.MultiIndex) else xs[current_hours[0]:]], 'trading-hours'
+    else:
+        lc = __closes_series.loc[df.iloc[0].timestamp.date(): most_recent.date()][::-1]
 
-        if not result.empty:
-            return result, period
+        for i in range(len(lc)):
+            result = df.loc[xs[lc.iloc[i]:, :] if isinstance(df.index, pd.MultiIndex) else xs[lc.iloc[i]:]]
+            if not result.empty:
+                period = 'after-hours'
+                break
+        else:
+            result, period = df, 'after-hours'
+
+    return result, period
--- tests/data/test_ts_utils.py
+++ tests/data/test_ts_utils.py
@@ -1,9 +1,12 @@
+import logging
 import random
 import unittest
 
 import atpy.data.tradingcalendar as tcal
+from atpy.backtesting.data_replay import DataReplay
 from atpy.data.iqfeed.iqfeed_history_provider import *
-from atpy.data.ts_util import set_periods, current_period
+from atpy.data.ts_util import current_period
+from atpy.data.ts_util import set_periods
 
 
 class TestTSUtils(unittest.TestCase):
@@ -83,3 +86,46 @@ class TestTSUtils(unittest.TestCase):
             slc, period = current_period(df)
             self.assertTrue(period in ('trading-hours', 'after-hours'))
             self.assertGreater(len(df), len(slc))
+
+    def test_current_period_2(self):
+        logging.basicConfig(level=logging.DEBUG)
+
+        batch_len = 10000
+        batch_width = 2000
+
+        l1, l2 = list(), list()
+        with IQFeedHistoryProvider() as provider, DataReplay().add_source(iter(l1), 'e1', historical_depth=1000) as dr:
+            now = datetime.datetime.now()
+            df = provider.request_data(BarsFilter(ticker="AAPL", interval_len=60, interval_type='s', max_bars=batch_len), adjust_data=False, sync_timestamps=False)
+
+            dfs1 = {'AAPL': df}
+            for i in range(batch_width):
+                dfs1['AAPL_' + str(i)] = df.sample(random.randint(int(len(df) / 3), len(df) - 1))
+
+            df = pd.concat(dfs1).swaplevel(0, 1)
+            df.reset_index(level=1, inplace=True)
+            df.sort_index(inplace=True)
+            df.set_index('level_1', drop=False, append=True, inplace=True)
+            l1.append(df)
+
+            logging.getLogger(__name__).debug('Random data generated in ' + str(datetime.datetime.now() - now) + ' with shapes ' + str(df.shape))
+
+            now = datetime.datetime.now()
+
+            for i, r in enumerate(dr):
+                if i % 1000 == 0 and i > 0:
+                    new_now = datetime.datetime.now()
+                    elapsed = new_now - now
+                    logging.getLogger(__name__).debug('Time elapsed ' + str(elapsed) + ' for ' + str(i) + ' iterations; ' + str(elapsed / 1000) + ' per iteration')
+                    self.assertGreater(10000, (elapsed / 1000).microseconds)
+                    now = new_now
+
+                for e in r:
+                    period, _ = current_period(r[e])
+                    if len(period) >= len(r[e]):
+                        data, period = current_period(r[e])
+                        self.assertTrue(not data.empty)
+                        self.assertTrue(period in ('trading-hours', 'after-hours'))
+
+            elapsed = datetime.datetime.now() - now
+            logging.getLogger(__name__).debug('Time elapsed ' + str(elapsed) + ' for ' + str(i + 1) + ' iterations; ' + str(elapsed / (i % 1000)) + ' per iteration')
