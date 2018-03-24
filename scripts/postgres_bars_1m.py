88128df215450587602e7429f2ef2e08a5126cb4
Ivan Vasilev <ivanvasilev@gmail.com>
2018-03-24 16:41:18 +0200

    scripts to build datasets added

---
+++ scripts/postgres_bars_1m.py
@@ -0,0 +1,21 @@
+import argparse
+import logging
+import os
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description="PostgreSQL and IQFeed configuration")
+    parser.add_argument('-drop', action='store_true', help="Drop the database")
+    args = parser.parse_args()
+
+    query = "python3 ../atpy/data/iqfeed/update_postgres_cache.py " + \
+            ("-drop" if args.drop else "") + \
+            " -url='" + os.environ['POSTGRESQL_CACHE'] + "'" + \
+            " -update_bars" + \
+            " -table_name='bars_1m'" + \
+            " -interval_len=60" + \
+            " -interval_type='s'" + \
+            " -skip_if_older=30"
+
+    os.system(query)
